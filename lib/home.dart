95844e8e66586b3e34a09e344d33f049c0dcb1cf
Rusiru_Chapana <rusiruchapana@gmail.com>
2024-12-18 07:57:20 +0000

    #

--- lib/home.dart
+++ lib/home.dart
@@ -6,11 +6,33 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text("Title")),
-        backgroundColor: Colors.amberAccent,
-      ),
-      body: const Text("Body"),
-    );
+        appBar: AppBar(
+          title: const Center(child: Text("Title")),
+          backgroundColor: Colors.amberAccent,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Center(child: const Text("text1")),
+              width: 50,
+              height: 100,
+              color: Colors.redAccent,
+            ),
+            Container(
+              child: Center(child: const Text("text2")),
+              width: 75,
+              height: 100,
+              color: Colors.yellowAccent,
+            ),
+            Container(
+              child: Center(child: const Text("text3")),
+              width: 100,
+              height: 100,
+              color: Colors.blueAccent,
+            ),
+          ],
+        ));
   }
 }
