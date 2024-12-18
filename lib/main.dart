057c6f9986fb5709357478a5aad30a5284d82d71
Rusiru_Chapana <rusiruchapana@gmail.com>
2024-12-18 04:27:18 +0000

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Title"),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: const Home(),
+    ),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Rusiru chapana",
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
 }
