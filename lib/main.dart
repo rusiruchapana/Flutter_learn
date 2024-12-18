9411e822f08b573c129c8689554435f13a2592f8
Rusiru_Chapana <rusiruchapana@gmail.com>
2024-12-18 06:15:36 +0000

    refactor the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,6 @@
+import 'package:cofee_card/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Title"),
-        backgroundColor: Colors.amberAccent,
-      ),
-      body: const Home(),
-    ),
-  ));
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.limeAccent,
-        alignment: Alignment.center,
-        width: 200,
-        height: 100,
-        child: const Text(
-          "Rusiru chapana jayampathi",
-          style: TextStyle(fontSize: 18, color: Colors.blueAccent),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-  }
+  runApp(const MaterialApp(home: Home()));
 }
